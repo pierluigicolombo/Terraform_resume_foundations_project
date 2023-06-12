81e0ce4731cf2c80cba3622d0d894fd0a28b85ee
pierluigi <pierlucolombo@gmail.com>
2023-06-12 20:48:50 +0200

    added resource group creation in main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=2.91.0"
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
     }
   }
 }
 
 provider "azurerm" {
-  feature {}
+  features {}
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "East Us"
+  tags = {
+    environment = "dev"
+  }
 }
