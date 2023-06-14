397d26632e6252e10a65e1f2469feebcecee519c
pierluigi <pierlucolombo@gmail.com>
2023-06-14 16:12:22 +0200

    added network security group and rule

--- main.tf
+++ main.tf
@@ -36,3 +36,29 @@ resource "azurerm_subnet" "mtc-subnet" {
   virtual_network_name = azurerm_virtual_network.mtc-vn.name
   address_prefixes     = ["10.123.1.0/24"]
 }
+
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_virtual_network.mtc-vn.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+} 
